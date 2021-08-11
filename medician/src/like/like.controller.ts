516a7c3c7f4133add88f5bcf918fc57b2b4db347
LynChoi27 <81853832+LynChoi27@users.noreply.github.com>
2021-08-11 22:55:53 +0900

    like module commit

--- medician/src/like/like.controller.ts
+++ medician/src/like/like.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { LikeService } from './like.service';
 
 @Controller('like')
-export class LikeController {}
+export class LikeController {
+    constructor(private readonly likeService: LikeService) {}
+
+    @Get('/addItem')
+    getHello(@Query('id') id : number): string {
+        console.log(id);
+    return this.likeService.getAdd(id);
+    }
+
+}
--- medician/src/like/like.service.ts
+++ medician/src/like/like.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class LikeService {}
+export class LikeService {
+
+    getAdd(id : number): string {
+        return 'add ';
+      }   
+
+
+
+}
