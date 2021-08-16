44853e86e18c474ad940d796fe3644579f93b2f5
beomjin <qjawls403@gmail.com>
2021-08-16 16:59:16 +0900

    add product info files (json)
    Signed-off-by: beomjin <qjawls403@gmail.com>

--- medician/src/search/product-loader.service.ts
+++ medician/src/search/product-loader.service.ts
@@ -1,16 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ValidationPipe } from '@nestjs/common';
 import { ProductInfo } from './entities/search.entities';
+import * as fs from 'fs'
+import { classToClass } from 'class-transformer';
 
 @Injectable()
 export class ProductLoaderService {
     private productList: ProductInfo[] = [];
+    private tempProduct: ProductInfo;
+    private fileDir: string = 'temp-database/products/';
 
-    getProductList(): ProductInfo[] {
-        return this.productList;
-    }
-
-    getProductByIndex(index: number): ProductInfo {
-        return this.productList.find(element => element.index === index);
+    constructor() {
+        var tempFileList: any;
+        // read file list
+        tempFileList = fs.readdirSync(this.fileDir);
+        // read all json files
+        tempFileList.forEach(element => {
+            if (RegExp('\.json$', 'i').test(element)) {
+                this.tempProduct = JSON.parse(fs.readFileSync(this.fileDir + element, 'utf8'));
+                this.addProduct(this.tempProduct);
+            }
+        });
     }
 
     addProduct(aProduct: ProductInfo) {
@@ -18,6 +27,14 @@ export class ProductLoaderService {
             index: this.productList.length + 1,
             ...aProduct,
         });
+    }
+
+    getProductList(): ProductInfo[] {
+        return this.productList;
+    }
+
+    getProductByIndex(index: number): ProductInfo {
+        return this.productList.find(element => element.index === index);
     }
 
     deleteProduct(index: number) {
